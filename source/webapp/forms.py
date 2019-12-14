ea1a8ef6c8e64011523ffc7a81c215d4d50181e9
Айсулу <aisuluueco2009@yandex.ru>
2019-12-14 20:58:21 +0600

    Исправила ошибку при создании фото.

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -1,16 +1,9 @@
 from django import forms
-from webapp.models import Image, Comment
+from webapp.models import Image
 
 
 class PhotoForm(forms.ModelForm):
     class Meta:
         model = Image
-        exclude = ['create_at']
-
-
-class ImageCommentForm(forms.ModelForm):
-    class Meta:
-        model = Comment
-        fields = ['text']
-        exclude =['author_comment', 'create_comment']
+        exclude = ['create_at', 'author']
 
